3776aae5b807cb8cace3d773f7a6fe51f18c7749
Matias Sendra 2 <matu2982@hotmail.com>
2026-08-01 19:09:18 -0300

    fix: sidebar admin - padding 0, line-height 20px para alinear iconos con texto

--- ejecutar12.sh
+++ ejecutar12.sh
@@ -2,5 +2,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /var/www/html/BACKEND/AGROVANTS/angular12original/front-end
-nvm use 22
-ng serve
+nvm use 16
+ng serve --port 4201
